11286193bf699eb8e07553d75531542a6949b881
marco <marcoranieri0451@gmail.com>
2022-06-18 16:19:24 +0200

    aggiunta conversione secondo numero da stringa a intero + fix nome variabile

--- MCD/mcd.s
+++ MCD/mcd.s
@@ -106,5 +106,44 @@ loop1:
 
 fine_str_to_num1:
 
-    movl %eax, num1_int             # salvo il valore numerico trovato nella variabile num1_int
+    movl %eax, num1             # salvo il valore numerico trovato nella variabile num1
+
+
+# secondo numero
+str_to_num2:
+
+    leal num2_str, %esi             # carico l'indirizzo della variabile num2_str in %esi
+
+    xorl %eax, %eax                 # resetto a zero tutti i registri primari
+    xorl %ebx, %ebx
+    xorl %ecx, %ecx
+    xorl %edx, %edx
+
+loop2:
+
+    movb (%ecx, %esi, 1), %bl       # muovo ciò che sta all'indirizzo %ecx%esi*1, in %bl 
+                                    # ( => al primo ciclo, la prima lettera della stringa)
+                                    # (nei cicli successivi, man mano incremento %ecx di 1 quindi passo alle lettere successive)
+    cmp $10, %bl                    # verifico se il carattere "\n" è stato letto (ascii 10 = line feed LF)
+
+    je fine_str_to_num2             # se sì, salta a fine loop
+
+    subb $48, %bl                   # converte cifra da string a numero in ebx
+
+    movl $10, %edx                  # mette il valore 10 in %edx
+    mulb %dl                        # dl è 1 byte => moltiplica al per dl e mette il risultato in ax (al primo giro sarà zero)
+    addl %ebx, %eax                 # sommo ebx ad eax e lo salvo in eax, a disposizione per il prossimo giro (verrà moltipicato per 10 prima ceh gli venga sommato il nuovo ebx)
+
+    inc %ecx                        # incremento di 1 ecx per passare alla cifra successiva della stringa
+    jmp loop2                       # ricomincio il ciclo
+
+
+fine_str_to_num2:
+
+    movl %eax, num2             # salvo il valore numerico trovato nella variabile num2
+
+
+# ----- CALCOLO IL MCD -------
+
+
 
